39d856827a384b4febc21ecea9f0357d603f2b8f
Melvin Sparkman <93449867+melvinSparkMan@users.noreply.github.com>
2022-02-10 15:14:39 -0600

    Thymeleaf Set-Up

--- src/main/java/org/launchcode/hellospring/HelloSpringApplication.java
+++ src/main/java/org/launchcode/hellospring/HelloSpringApplication.java
@@ -3,11 +3,15 @@ package org.launchcode.hellospring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class HelloSpringApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloSpringApplication.class, args);
-	}
-
-}
+//@SpringBootApplication
+//public class HelloSpringApplication {
+//
+//	public static void main(String[] args) {
+//
+//		SpringApplication.run(HelloSpringApplication.class, args);
+//	}
+//
+//}
